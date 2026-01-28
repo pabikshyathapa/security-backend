d790d537e5080064b99ed56a5b3314292c2d1cff
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-28 17:03:25 +0545

    changes

--- routes/oderRoute.js
+++ routes/oderRoute.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const orderController = require("../controllers/ordercontroller");
 const { authenticate } = require("../middlewares/authorizedUser");
 
-/* USER ROUTES */
 router.post("/create", authenticate, orderController.createOrder);
 router.get("/my-orders", authenticate, orderController.getMyOrders);
 
-/* ESEWA CALLBACK - NO CSRF, NO AUTH */
 router.get("/verify-esewa", orderController.verifyEsewaOrder);
 
-/* ADMIN ROUTE */
 router.get("/all", authenticate, orderController.getAllOrders);
 
 module.exports = router;
