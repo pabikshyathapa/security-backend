84052962d8eb3ad8e324cecaf8dbb32ec3382b83
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-28 17:01:27 +0545

    changes

--- controllers/publicProductController.js
+++ controllers/publicProductController.js
@@ -17,20 +17,3 @@ exports.getAllUserProducts = async (req, res) => {
 }
 
 
-// const Product = require('../models/Product')
-
-// exports.getPublicProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find()
-//       .populate('categoryId', 'name')
-//       .populate('sellerId', 'name email')
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Products fetched successfully",
-//       data: products,
-//     })
-//   } catch (err) {
-//     res.status(500).json({ success: false, message: "Server error" })
-//   }
-// }
