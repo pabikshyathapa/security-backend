3a9729ede70c7763a518ad041bda44e9085db4ef
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-28 17:15:22 +0545

    changes

--- routes/admin/categoryRouteAdmin.js
+++ routes/admin/categoryRouteAdmin.js
@@ -4,31 +4,25 @@ const categoryController = require('../../controllers/admin/categorymanagement')
 const upload = require("../../middlewares/fileuplaod");
 const { authenticate, isAdmin } = require("../../middlewares/authorizedUser");
 
-// Apply authentication and admin check to ALL routes
 router.use(authenticate);
 router.use(isAdmin);
 
-// Create new category (Admin only)
 router.post(
     '/', 
     upload.single("image"),
     categoryController.createCategory
 );
 
-// Get all categories (Admin only)
 router.get('/', categoryController.getAllCategories);
 
-// Get single category by ID (Admin only)
 router.get('/:id', categoryController.getCategoryById);
 
-// Update category (Admin only)
 router.put(
     '/:id', 
     upload.single("image"),
     categoryController.updateCategory
 );
 
-// Delete category (Admin only)
 router.delete('/:id', categoryController.deleteCategory);
 
 module.exports = router;
